9f9e36c579fa8b34f1c531bad09f71dd0691f7d1
Mark Shackelford <mshackelford@apple.com>
2016-06-17 10:50:04 -0700

    Initial Commit

---
+++ Workout iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Workout iOS
+//
+//  Created by Mark Shackelford on 6/17/16.
+//  Copyright © 2016 TheShack Apps. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
